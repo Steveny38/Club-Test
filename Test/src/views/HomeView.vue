7edb8c443677c7f61bd7e3691f58ecd2bb54037d
Steveny38 <90789088+Steveny38@users.noreply.github.com>
2022-11-07 12:14:43 -0500

    asdf

---
+++ Test/src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div id="g_id_onload"
+         data-client_id="YOUR_GOOGLE_CLIENT_ID"
+         data-login_uri="https://your.domain/your_login_endpoint"
+         data-auto_prompt="false">
+      </div>
+      <div class="g_id_signin"
+         data-type="standard"
+         data-size="large"
+         data-theme="outline"
+         data-text="sign_in_with"
+         data-shape="rectangular"
+         data-logo_alignment="left">
+      </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
